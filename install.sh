4d02c1aee3d95b2f33a08248f2aa74a913b45574
Justin Fortunato <jfortunato25@gmail.com>
2020-04-29 15:00:10 -0400

    add default on-my-zsh .zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -sf $HOME/.dotfiles/.bashrc $HOME/.bashrc
+ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/.vim $HOME/.vim
 ln -sf $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/.tmux $HOME/.tmux
