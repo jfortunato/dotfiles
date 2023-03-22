1585bfd4ecff436087770fd1a0f2d7471257bb99
Justin Fortunato <jfortunato25@gmail.com>
2023-03-22 09:16:55 -0400

    Add and configure the powerlevel10k theme for zsh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -sf $HOME/.dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
 ln -sf $HOME/.dotfiles/.vim $HOME/.vim
 ln -sf $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/.tmux $HOME/.tmux
