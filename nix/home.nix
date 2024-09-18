99a35409412b079cee1adf63a2ec4c36a0b3ee2d
Justin Fortunato <developer@jfortunato.com>
2024-09-18 11:55:50 -0400

    Use subdirector for xdg config

--- nix/home.nix
+++ nix/home.nix
@@ -12,6 +12,7 @@
     })
     ./gnome
     ./shell
+    ./xdg
   ];
 
   nixGL.prefix = "${inputs.nixGL.packages."${pkgs.system}".nixGLIntel}/bin/nixGLIntel";
@@ -121,24 +122,6 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
-
-  xdg = {
-    enable = true;
-    configFile = {
-      "vim/vimrc".source = ../vim/vimrc;
-      "vim/after/syntax/html.vim".source = ../vim/after/syntax/html.vim;
-      "vim/after/syntax/htmljinja.vim".source = ../vim/after/syntax/htmljinja.vim;
-      "vim/colors/molokai.vim".source = ../vim/colors/molokai.vim;
-      "vim/snippets/html.snippets".source = ../vim/snippets/html.snippets;
-      "vim/snippets/javascript.snippets".source = ../vim/snippets/javascript.snippets;
-      "vim/snippets/php.snippets".source = ../vim/snippets/php.snippets;
-      "ideavim/ideavimrc".source = ../ideavim/ideavimrc;
-      "git/config".source = ../git/config;
-      "git/ignore".source = ../git/ignore;
-      "tmux/tmux.conf".source = ../tmux/tmux.conf;
-      "tmux/layout.conf".source = ../tmux/layout.conf;
-    };
   };
 
   # Home Manager can also manage your environment variables through
