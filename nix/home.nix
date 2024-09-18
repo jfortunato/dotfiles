e8026730df644c979a2f4f5c88d97dee3c5b44cc
Justin Fortunato <developer@jfortunato.com>
2024-09-18 11:07:03 -0400

    Place all nix stuff in nix subdirectory

--- nix/home.nix
+++ nix/home.nix
@@ -123,14 +123,14 @@
   xdg = {
     enable = true;
     configFile = {
-      "vim/vimrc".source = ./vim/vimrc;
-      "vim/after/syntax/html.vim".source = ./vim/after/syntax/html.vim;
-      "vim/after/syntax/htmljinja.vim".source = ./vim/after/syntax/htmljinja.vim;
-      "vim/colors/molokai.vim".source = ./vim/colors/molokai.vim;
-      "vim/snippets/html.snippets".source = ./vim/snippets/html.snippets;
-      "vim/snippets/javascript.snippets".source = ./vim/snippets/javascript.snippets;
-      "vim/snippets/php.snippets".source = ./vim/snippets/php.snippets;
-      "ideavim/ideavimrc".source = ./ideavim/ideavimrc;
+      "vim/vimrc".source = ../vim/vimrc;
+      "vim/after/syntax/html.vim".source = ../vim/after/syntax/html.vim;
+      "vim/after/syntax/htmljinja.vim".source = ../vim/after/syntax/htmljinja.vim;
+      "vim/colors/molokai.vim".source = ../vim/colors/molokai.vim;
+      "vim/snippets/html.snippets".source = ../vim/snippets/html.snippets;
+      "vim/snippets/javascript.snippets".source = ../vim/snippets/javascript.snippets;
+      "vim/snippets/php.snippets".source = ../vim/snippets/php.snippets;
+      "ideavim/ideavimrc".source = ../ideavim/ideavimrc;
     };
   };
 
