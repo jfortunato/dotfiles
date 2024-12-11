fa4817cf435da87778fca4ef39b7d97666d51f3d
Justin Fortunato <developer@jfortunato.com>
2024-12-11 09:39:13 -0500

    feat(go): Use XDG dirs for go paths

--- nix/modules/home-manager/home.nix
+++ nix/modules/home-manager/home.nix
@@ -144,8 +144,9 @@
      EDITOR = "nvim";
      # Set the FLAKE environment variable for `nh` (nix-helper)
      FLAKE = "/home/justin/.dotfiles/nix";
-     # Place go binaries installed with `go install` in this directory (instead of the default `~/go/bin`)
-     GOBIN = "${config.home.homeDirectory}/.local/bin";
+     GOBIN = "${config.home.homeDirectory}/.local/bin"; # Place go binaries installed with `go install` in this directory (instead of the default `~/go/bin`)
+     GOPATH = "${config.xdg.dataHome}/go"; # Use an XDG directory for GOPATH (instead of the default `~/go`)
+     GOMODCACHE = "${config.xdg.cacheHome}/go/mod"; # Use an XDG directory for GOCACHE (instead of the default `$GOPATH/pkg/mod`)
   };
 
   home.sessionPath = [
