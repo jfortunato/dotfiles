0881f8d419f5ee829358541239827e2928058c9d
Justin Fortunato <developer@jfortunato.com>
2025-01-31 16:07:28 -0500

    feat(nix): Clear /tmp on boot

--- nix/modules/nixos/system.nix
+++ nix/modules/nixos/system.nix
@@ -11,6 +11,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  boot.tmp.cleanOnBoot = true;
+
   # Enable networking
   networking.networkmanager.enable = true;
 
