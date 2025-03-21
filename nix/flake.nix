051363cae9113a7ccee9a893b5019926f5d01ec7
Justin Fortunato <developer@jfortunato.com>
2025-03-21 08:52:35 -0400

    feat(nixos): Reuse same specialArgs with HM extraSpecialArgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -27,26 +27,26 @@
   outputs = { nixpkgs, home-manager, nixgl, disko, ... }@inputs:
     {
       nixosConfigurations = {
-        ct14-a4 = nixpkgs.lib.nixosSystem {
+        ct14-a4 = nixpkgs.lib.nixosSystem rec {
           specialArgs = { inherit inputs; };
             modules = [
               ./hosts/ct14-a4
               # Home Manager as a module configuration. When using Home Manager on a NixOS system,
               # we can apply these configurations with `sudo nixos-rebuild switch --flake .#ct14-a4`.
               home-manager.nixosModules.default {
-                home-manager.extraSpecialArgs = { inherit inputs; };
+                home-manager.extraSpecialArgs = specialArgs;
                 home-manager.users."justin" = import ./hosts/ct14-a4/home.nix;
               }
             ];
         };
-        nixos-x1c6 = nixpkgs.lib.nixosSystem {
+        nixos-x1c6 = nixpkgs.lib.nixosSystem rec {
           specialArgs = { inherit inputs; };
             modules = [
               ./hosts/nixos-x1c6
               # Home Manager as a module configuration. When using Home Manager on a NixOS system,
               # we can apply these configurations with `sudo nixos-rebuild switch --flake .#nixos-x1c6`.
               home-manager.nixosModules.default {
-                home-manager.extraSpecialArgs = { inherit inputs; };
+                home-manager.extraSpecialArgs = specialArgs;
                 home-manager.users."justin" = import ./hosts/nixos-x1c6/home.nix;
               }
             ];
