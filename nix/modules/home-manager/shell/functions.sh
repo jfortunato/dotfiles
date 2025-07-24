80fa0bc2208bc5f912e941c93a431fb5fe8e18c5
Justin Fortunato <developer@jfortunato.com>
2025-07-24 09:27:12 -0400

    feat(shell): Create a live_grep shell function

--- nix/modules/home-manager/shell/functions.sh
+++ nix/modules/home-manager/shell/functions.sh
@@ -54,3 +54,22 @@ mirror-website() {
 find-recently-modified() {
     fd . $1 --type file --exec stat --format '%Y :%y %n' | sort -n | cut -d: -f2-
 }
+# Similar to what I use in fzf-lua: live_grep, but directly in the terminal.
+# See: https://junegunn.github.io/fzf/tips/ripgrep-integration/
+live_grep() (
+  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            nvim {1} +{2}     # No selection. Open the current line in Vim.
+          else
+            nvim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+  fzf --disabled --ansi --multi \
+      --bind "start:$RELOAD" --bind "change:$RELOAD" \
+      --bind "enter:become:$OPENER" \
+      --bind "ctrl-o:execute:$OPENER" \
+      --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+      --delimiter : \
+      --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+      --preview-window '~4,+{2}+4/3,<80(up)' \
+      --query "$*"
+)
