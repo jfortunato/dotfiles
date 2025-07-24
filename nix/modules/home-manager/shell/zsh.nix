c2f222d579579d777454ea1c6009390455c85fb2
Justin Fortunato <developer@jfortunato.com>
2025-07-24 09:26:38 -0400

    feat(zsh): Stop using oh-my-zsh

--- nix/modules/home-manager/shell/zsh.nix
+++ nix/modules/home-manager/shell/zsh.nix
@@ -7,11 +7,10 @@
     history = {
       size = 100000;
     };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "docker" "docker-compose" "vi-mode" "tmux" "fzf" ];
-    };
     initContent = ''
+      # Enable compatibility with bash's completion system.
+      autoload -U bashcompinit && bashcompinit
+
       source ${./aliases.sh}
       source ${./functions.sh}
 
@@ -19,18 +18,24 @@
       setopt nolist_beep
       setopt bash_auto_list
 
+      # Enable vi mode. Keep in mind when debugging that bindkey -v may not actually be
+      # necessary since it will automatically by set if $VISUAL or $EDITOR contains the
+      # string "vi" (e.g. if you set EDITOR=nvim or VISUAL=nvim. See `man zshzle`).
+      bindkey -v
+      # Ensure pressing backspace in vi mode deletes the character before the cursor
+      bindkey -v '^?' backward-delete-char
+
+      # Set up fzf key bindings and fuzzy completion
+      source <(fzf --zsh)
+
       # use Ctrl-P instead of Ctrl-T for fuzzy file selection
+      bindkey -r '^t'
       bindkey '^p' fzf-file-widget
-      # The oh-my-zsh fzf plugin already sets FZF_DEFAULT_COMMAND to use 'fd', but
-      # we want to use 'fd' for the fzf-file-widget as well.
-      export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+      # Use 'fd' for fuzzy file selection. using Ctrl-P
+      export FZF_CTRL_T_COMMAND="fd --type f --hidden --exclude .git"
 
       # use Ctrl+Space to complete the suggestion from zsh-autosuggestions
       bindkey '^ ' forward-char
-
-      # Enable short-option completion stacking for example: docker exec -it <tab>
-      zstyle ':completion:*:*:docker:*' option-stacking yes
-      zstyle ':completion:*:*:docker-*:*' option-stacking yes
     '';
   };
 }
