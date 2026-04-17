dd02846a03fc36e875b36ec9a3c141f8d5c9023b
Justin Fortunato <developer@jfortunato.com>
2026-04-17 14:54:57 -0400

    feat(nvim): Update colorscheme from "tomorrrow_night" to "monokai_ristretto"

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -1,7 +1,7 @@
 -- =====
 -- THEME
 -- =====
-vim.cmd.colorscheme("tomorrow_night")
+vim.cmd.colorscheme("monokai_ristretto")
 -- Fix broken status bar colors for this theme after updating nvim https://github.com/nvim-lualine/lualine.nvim/issues/1312
 vim.api.nvim_set_hl(0, "StatusLine", {reverse = false})
 vim.api.nvim_set_hl(0, "StatusLineNC", {reverse = false})
