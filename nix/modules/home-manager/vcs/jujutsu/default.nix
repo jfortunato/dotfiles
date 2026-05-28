813e7f1c756132f2623cf9a6a0dc073118a97b1f
Justin Fortunato <developer@jfortunato.com>
2026-05-28 16:59:51 -0400

    feat(starship): Use starship-jj package from unstable instead of custom flake

--- nix/modules/home-manager/vcs/jujutsu/default.nix
+++ nix/modules/home-manager/vcs/jujutsu/default.nix
@@ -108,6 +108,6 @@
         when = true;
       };
     };
-    home.packages = [ inputs.starship-jj.packages.${pkgs.stdenv.hostPlatform.system}.starship-jj ];
+    home.packages = [ pkgs-unstable.starship-jj ];
   };
 }
